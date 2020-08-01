78109ae0ab37e34a9852615ccf5f913496af2590
brant98 <pulang516@163.com>
2020-08-01 10:40:01 +0800

    done

--- test_kEx.c
+++ test_kEx.c
@@ -1,3 +1,4 @@
+#include<time.h>
 #include"SM2.h"
 #include"miracl.h"
 int test_kEx(void)
@@ -14,23 +15,40 @@ int test_kEx(void)
 	int i = 0;
 	epoint* RA,*RB,*pA,*pB,*V;  //公钥
 	big rA,rB,dA, dB;
-	
+	clock_t start, finish;//计算运行时间用
+	start = clock();
 	SM2_init();//椭圆曲线初始化
 	SM2_creat_key(&dA, &pA); //创建AB的公、私钥
 	SM2_creat_key(&dB, &pB);
 	
 	
 	//计算RA  RB rA rB
+	
+
 	cal_RA_RB(&RA, &rA);
 	cal_RA_RB(&RB,&rB);
-	
+
 	//计算ZA ZB
 	SM2_ZA(pA, IDA, ZA);//计算用户A、B的标识
 	SM2_ZA(pB, IDB, ZB);
 
 	//发起者   hash中放的SB
+	for (i = 0; i < 1000; i++)
+	{
 	B1(&V,RA, RB, pA, pB, dB, rB, ZA, ZB, KB, SB);
 	A2(RA, rA, dA, RB, pB, ZA, ZB, KA, SB,SA);
 	B2(V, RA, RB, ZA, ZB, SA);
+
+	}
+
+
+
+	
+	finish = clock();
+	printf("Test of this algorithm finished\n");
+	printf("Start at  %f s\n", (double)start / CLOCKS_PER_SEC);
+	printf("End at %f s\n", (double)finish / CLOCKS_PER_SEC);
+	printf("1000 times tests  used %f seconds in total.\n", (double)difftime(finish, start) / CLOCKS_PER_SEC);
+	printf("The algorithm runs once used %f seconds on average.\n", (double)difftime(finish, start) / CLOCKS_PER_SEC / 1000);
 	return 1;//返回1表示成功
 }
